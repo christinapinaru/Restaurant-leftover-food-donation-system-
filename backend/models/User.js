f2f403041cf6459b25a8b68be59793461e66b0e7
christinapinaru <christinasornapinaru@gmail.com>
2026-07-13 22:29:10 +0600

    Add role field to User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,21 +1,29 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
 
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+    name:{
+        type:String,
+        required:true
+    },
 
-  password: {
-    type: String,
-    required: true,
-  },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    },
+
+    role: {
+  type: String,
+  enum: ["admin", "donor", "recipient"],
+  default: "recipient",
+},
+
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User",userSchema);
